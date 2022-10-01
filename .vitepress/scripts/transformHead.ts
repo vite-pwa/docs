de4ae6e2475e636add26b93a7ca8fecf699b1bdf
userquin <userquin@gmail.com>
2022-10-01 13:48:26 +0200

    chore: add prefetch and non blocking fira font headers

--- .vitepress/scripts/transformHead.ts
+++ .vitepress/scripts/transformHead.ts
@@ -1,7 +1,17 @@
 import type { HeadConfig, TransformContext } from 'vitepress'
 
+const firaFont = 'https://fonts.googleapis.com/css2?family=Fira+Code&display=swap'
+const googleapis = 'https://fonts.googleapis.com'
+const gstatic = 'https://fonts.gstatic.com'
+
 export const transformHead = async ({ pageData }: TransformContext): Promise<HeadConfig[]> => {
   const head: HeadConfig[] = []
+  head.push(['link', { rel: 'dns-prefetch', href: googleapis }])
+  head.push(['link', { rel: 'dns-prefetch', href: gstatic }])
+  head.push(['link', { rel: 'preconnect', crossorigin: 'anonymous', href: googleapis }])
+  head.push(['link', { rel: 'preconnect', crossorigin: 'anonymous', href: gstatic }])
+  head.push(['link', { rel: 'preload', as: 'style', onload: 'this.onload=null;this.rel=\'stylesheet\'', href: firaFont }])
+  head.push(['noscript', {}, `<link rel="stylesheet" crossorigin="anonymous" href="${firaFont}" />`])
   head.push(['link', { rel: 'prefetch', href: '/icon_light.svg' }])
   head.push(['link', { rel: 'prefetch', href: '/icon_dark.svg' }])
   if (pageData.relativePath === 'index.md') {
