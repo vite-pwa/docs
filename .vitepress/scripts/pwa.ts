8933394e495b089a239b9a45920ece0d99424a03
userquin <userquin@gmail.com>
2023-10-15 14:55:42 +0200

    docs: bump to vite-pwa/vitepress v0.2.2

--- .vitepress/scripts/pwa.ts
+++ .vitepress/scripts/pwa.ts
@@ -1,6 +1,6 @@
-import type { VitePWAOptions } from 'vite-plugin-pwa'
+import type { PwaOptions } from '@vite-pwa/vitepress'
 
-export const pwa: Partial<VitePWAOptions> = {
+export const pwa: Partial<PwaOptions> = {
   outDir: '.vitepress/dist',
   registerType: 'prompt',
   includeManifestIcons: false,
@@ -100,6 +100,9 @@ export const pwa: Partial<VitePWAOptions> = {
     edge_side_panel: {
       preferred_width: 480,
     },
+  },
+  experimental: {
+    includeAllowlist: true,
   },
   workbox: {
     globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2,excalidraw}'],
