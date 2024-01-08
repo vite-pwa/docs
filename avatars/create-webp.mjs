542ec20054bf2897bcbb2ccb2c7d4bd03631a5ce
userquin <userquin@gmail.com>
2024-01-08 18:57:44 +0100

    chore: use only webp + script to generate them

---
+++ avatars/create-webp.mjs
@@ -0,0 +1,14 @@
+import sharp from 'sharp'
+import fg from 'fast-glob'
+
+async function createWebp() {
+  /** @type {string[]} */
+  const images = await fg('./avatars/*.png', { absolute: false })
+  for (const image of images) {
+    await sharp(image).resize({ width: 128, height: 128 }).webp({ quality: 100 }).toFile(
+      image.replace('./avatars/', './public/team-avatars/').replace('.png', '.webp'),
+    )
+  }
+}
+
+createWebp()
